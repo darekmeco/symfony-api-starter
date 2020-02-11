e40b18d35cb0387d83850d9d63fa3bda7cd44060
Dariusz Męciński <dariusz.mecinski@gmail.com>
2020-02-11 22:12:28 +0100

    init

---
+++ src/Controller/ResumeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Repository\ResumeRepository;
+
+
+class ResumeController extends AbstractController
+{
+    private $repository;
+    public function __construct(ResumeRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+    * @Route("/resumes", name="get_all_resumes", methods={"GET"})
+    */
+    public function all(): JsonResponse
+    {
+        $resumes = $this->repository->findAll();
+        $data = [];
+
+        foreach ($resumes as $resume) {
+            $data[] = [
+            'id' => $resume->getId(),
+            'firstName' => $resume->getFirstName(),
+            'lastName' => $resume->getLastName(),
+            'position' => $resume->getPosition()
+        ];
+        }
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+}
